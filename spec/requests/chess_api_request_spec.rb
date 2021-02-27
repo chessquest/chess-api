eb8a478f98eb49a5634f9cf63463a544a995451d
Phil McCarthy <philmcc2631@gmail.com>
2021-02-27 12:31:22 -0700

    Add config.ru file to get Sinatra running on Heroku

--- spec/requests/chess_api_request_spec.rb
+++ spec/requests/chess_api_request_spec.rb
@@ -1,21 +1,21 @@
-require 'spec_helper'
-require 'json'
-require 'rspec'
-require './app/controllers/games_controller'
+# require 'spec_helper'
+# require 'json'
+# require 'rspec'
+# require './app/controllers/games_controller'
 
 
-def app
-  GamesController
-end
+# def app
+#   GamesController
+# end
 
-describe "Game endpoint" do
-  describe "Happy path" do
-    xit "Can get a response" do
+# describe "Game endpoint" do
+#   describe "Happy path" do
+#     xit "Can get a response" do
 
-      json_response = File.read('spec/fixtures/game.json')
+#       json_response = File.read('spec/fixtures/game.json')
 
-      stub_request(:get, "/api/v1/game/").
-      to_return(status: 200, body: json_response)
-    end
-  end
-end
+#       stub_request(:get, "/api/v1/game/").
+#       to_return(status: 200, body: json_response)
+#     end
+#   end
+# end
