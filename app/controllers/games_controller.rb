ffe57c6de30324596ac25c7b6ed5709b082d021a
Phil McCarthy <philmcc2631@gmail.com>
2021-02-27 13:41:04 -0700

    Set default content type for all responses to json

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,8 @@
 class GamesController < Sinatra::Base
+  before do
+    content_type 'application/json'
+  end
+  
   get '/api/v1/game' do
     search_params = params[:find_player]
     game = GamesFacade.find_game(search_params)
